0cea044633bbccda7e229d6d652f7cc1889fd532
tuanndl <tuanndl@remitano.com>
2022-11-20 19:46:27 +0700

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login, Dashboard, SignUp } from './views';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<div>Loading</div>}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
