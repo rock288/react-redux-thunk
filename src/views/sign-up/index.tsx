1b45b5292e154cd0b3c296e5915a7a435bedb0b9
tuanndl <tuanndl@remitano.com>
2022-11-21 19:35:43 +0700

    remove yarn

--- src/views/sign-up/index.tsx
+++ src/views/sign-up/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Row, Col, Container } from 'reactstrap';
 
-function DashBoard() {
-  return <div>Sign up</div>;
+function SignUp() {
+  return (
+    <Container fluid>
+      <Row className="row min-vh-100">
+        <Col className="d-none d-md-block banner-section" xs="0" md="6" lg="8"></Col>
+        <Col
+          xs="12"
+          md="6"
+          lg="4"
+          className="d-flex justify-content-center align-items-center p-4"
+        ></Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default DashBoard;
+export default SignUp;
